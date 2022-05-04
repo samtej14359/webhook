54fa8a715928805b7d23f758c9a87a42ea6b9c45
samtej14359 <101544409+samtej14359@users.noreply.github.com>
2022-05-04 10:41:40 +0530

    Update App.java

--- src/main/java/devopsclass/App.java
+++ src/main/java/devopsclass/App.java
@@ -9,5 +9,19 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World! this is sam " );
-    }
-}
+       System.out.println( "Hello World! this is my webhook" );
+       }
+}    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
